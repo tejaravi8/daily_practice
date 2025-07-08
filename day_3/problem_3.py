f25a8831d0aab3ac22872b8b09329a83168dc3e0
tejaravi8 <botsaraviteja@gmail.com>
2025-07-08 17:15:59 +0530

    Day3 Lists:Remove all negetive numbers in place

---
+++ day_3/problem_3.py
@@ -0,0 +1,15 @@
+# Remove All Negative Numbers in Place
+# Example: [1, -2, 3, -4] → [1, 3]
+
+nums=[1,-2,3,-4]
+non_neg=[]
+
+for i in nums:
+    if i>0:
+        non_neg.append(i)
+        
+print(non_neg)
+# nums.pop(1)
+# nums.pop(2)
+
+# print(nums)
